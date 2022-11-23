86e296df0196273e84ea66c3f491bb60e50976da
Crow <oneso123456789@gmail.com>
2022-11-23 17:28:34 +0900

    gom()작성 todo 펀치 작성-민종-

---
+++ src/main/java/io/springboot/githubtestproject/controller/TestController.java
@@ -0,0 +1,14 @@
+package io.springboot.githubtestproject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    // TODO: 2022-11-23 펀치적어야함 
+    /** 안녕안녕 */
+    public void gom(){
+        System.out.println("gomgom");
+    }
+
+}

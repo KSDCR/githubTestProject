50fe6790a19b1e18574aae0a3fb2b371fb3794ef
Crow <oneso123456789@gmail.com>
2022-11-23 19:15:36 +0900

    펀치 작성 완료 punch()-민종-

--- src/main/java/io/springboot/githubtestproject/controller/TestController.java
+++ src/main/java/io/springboot/githubtestproject/controller/TestController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    // TODO: 2022-11-23 펀치적어야함 
+
     /** 안녕안녕 */
     public void gom(){
         System.out.println("gomgom");
+    }
+
+    public void punch(){
+        System.out.println("punch");
     }
 
 }

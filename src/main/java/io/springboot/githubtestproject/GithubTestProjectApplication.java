0ea897114ec7bc81dfc6134d4cbc6ce3457ca970
Crow <oneso123456789@gmail.com>
2022-11-23 16:04:44 +0900

    Initial commit

---
+++ src/main/java/io/springboot/githubtestproject/GithubTestProjectApplication.java
@@ -0,0 +1,13 @@
+package io.springboot.githubtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springboot/githubtestproject/GithubTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.githubtestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
